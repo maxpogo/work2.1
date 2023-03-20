dd680537af7ab9fc3ba195d1b0225db718b8e11d
maxpogo <maxim.pogodaev.93@mail.ru>
2023-03-20 17:04:58 +0300

    .idea/.gitignore

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 330;
+        int mile = 20;
+        int bonus = amount/mile;
+
+        System.out.println("Итоговые бонусные мили: " + bonus );
+
+    }
+}
